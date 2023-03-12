c3967cb56942870423d3c6c70394361631e35265
himanshu9999git <hchimanshu9999@gmail.com>
2023-03-12 07:05:14 +0530

    Created Signup For New User

---
+++ server/controllers/auth.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import { createError } from "../error.js";
+
+export const signup = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ name: req.body.name });
+    if (user)
+      return next(
+        createError(409, "This email is already registered with an account!")
+      );
+
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+    const newUser = new User({ ...req.body, password: hash });
+
+    await newUser.save();
+    res.status(200).send("User has been created!");
+  } catch (err) {
+    next(err);
+  }
+};
