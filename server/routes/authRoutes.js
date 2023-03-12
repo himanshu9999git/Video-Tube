ad0880981270a110d2a78cff125b0838da3f9f84
himanshu9999git <hchimanshu9999@gmail.com>
2023-03-12 07:17:42 +0530

    Created Sign In

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import {signup } from "../controllers/auth.js";
+import {signup , signin} from "../controllers/auth.js";
 
 const router = express.Router();
 
 //CREATE A USER
 router.post("/signup", signup)
+
+//SIGN IN
+router.post("/signin", signin)
 
 export default router;
