7cb4dabcb8191b082db733fcc1aecc3fce7494db
himanshu9999git <hchimanshu9999@gmail.com>
2023-03-12 23:11:42 +0530

    minor changes

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,11 +3,11 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_KEY,
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  authDomain: "video-7801c.firebaseapp.com",
+  projectId: "video-7801c",
+  storageBucket: "video-7801c.appspot.com",
+  messagingSenderId: "130073115219",
+  appId: "1:130073115219:web:365035e1474abbe7fba4a6",
 };
 
 const app = initializeApp(firebaseConfig);
